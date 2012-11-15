a04d0155ba47ac2a030b243f86587456e3aabbb8
Josh Blum <josh@joshknows.com>
2012-11-15 01:22:45 -0800

    created tests dir and import test

---
+++ tests/import_test.py
@@ -0,0 +1,11 @@
+# Copyright (C) by Josh Blum. See LICENSE.txt for licensing information.
+
+import unittest
+
+class ImportTest(unittest.TestCase):
+
+    def test_import(self):
+        import grextras
+
+if __name__ == '__main__':
+    unittest.main()
