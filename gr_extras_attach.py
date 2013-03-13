62fa90da987578eaeaf4b00c216bfe984432e6dd
Nicholas Corgan <nick.corgan@ettus.com>
2013-03-13 15:35:54 -0700

    Renamed SubProj.cmake to ExtrasSubProj.cmake and edited Python script accordingly

--- gr_extras_attach.py
+++ gr_extras_attach.py
@@ -10,9 +10,9 @@ if __name__ == '__main__':
     top_gr_cmakelists = os.path.join(sys.argv[1], 'CMakeLists.txt')
     gr_extras_src_dir = os.path.dirname(os.path.abspath(__file__)).replace("\\","\\\\")
     content = open(top_gr_cmakelists).read()
-    if 'SubProj.cmake' not in content:
+    if 'ExtrasSubProj.cmake' not in content:
         content = content.replace('add_subdirectory(grc)',
 """add_subdirectory(grc)
 file(TO_CMAKE_PATH %s GR_EXTRAS_SRC_DIR)
-include(${GR_EXTRAS_SRC_DIR}/SubProj.cmake)"""%gr_extras_src_dir)
+include(${GR_EXTRAS_SRC_DIR}/ExtrasSubProj.cmake)"""%gr_extras_src_dir)
     open(top_gr_cmakelists, 'w').write(content)
