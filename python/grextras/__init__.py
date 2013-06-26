83bc6676b5160ab74e72d70dbfc9e94524ba4172
Josh Blum <josh@joshknows.com>
2013-06-25 18:12:43 -0700

    grextras: moved path fix into gras

--- python/grextras/__init__.py
+++ python/grextras/__init__.py
@@ -1,12 +1,6 @@
 # Copyright (C) by Josh Blum. See LICENSE.txt for licensing information.
 
 import gras
-
-#needed to find gras modules installed in subdir (swig module causes this)
-import os
-import sys
-sys.path.append(os.path.dirname(gras.__file__))
-
 from GrExtras_TestUtils import *
 from GrExtras_Misc import *
 from GrExtras_Ops import *
